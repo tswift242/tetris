tswift242/tetris

commit dcec42bbee3b983c4bbcd2a258f84f69be20b124

Fixes #5: remove magic numbers/values

src/main/java/com/tswift242/tetris/TetrisPanel.java
@@ -30,14 +30,25 @@ import com.tswift242.tetris.blocks.Sblock;
 import com.tswift242.tetris.blocks.Tblock;
 import com.tswift242.tetris.blocks.TetrisBlock;
 import com.tswift242.tetris.blocks.Zblock;
+import com.tswift242.tetris.config.TetrisProperties;
 
 public class TetrisPanel extends JPanel
 {
-	private final int WIDTH = 400, HEIGHT = 800; //window size
-	private final int INIT_DELAY = 400; //timer delay
-	private final int ROWS = 20, COLUMNS = 10; //grid size
-	private final int NUM_HIGH_SCORES = 10;
-	private final String MUSIC_FILE = "/12-hans_zimmer-time.wav";
+	private static final int NUM_UNIQUE_TETRIS_BLOCKS = 7;
+	// window size
+	private static final int WIDTH = 400;
+	private static final int HEIGHT = 800;
+	// grid size
+	private static final int ROWS = 20;
+	private static final int COLUMNS = 10;
+	private static final int INIT_DELAY = 400; //timer delay
+	// TODO: make this value formulaic instead of fixed in the future
+	private static final int SCORE_INCREMENT_VALUE = 100;
+	private static final int HIGH_SCORES_FILENAME = "TetrisHighScores.txt";
+	// maximum number of high scores to save
+	private static final int MAX_HIGH_SCORES = 10;
+	private static final String MUSIC_FILE = "/12-hans_zimmer-time.wav";
+	private static final Color TEXT_COLOR = Color.white;
 
 	private Timer timer;
 	private DirectionListener DL;
@@ -85,9 +96,9 @@ public class TetrisPanel extends JPanel
 		}
 
 		//keep track of high scores
-		HighScores = new int[NUM_HIGH_SCORES];
+		HighScores = new int[MAX_HIGH_SCORES];
 		Arrays.fill(HighScores, 0);
-		initials = new String[NUM_HIGH_SCORES];
+		initials = new String[MAX_HIGH_SCORES];
 		Arrays.fill(initials, "xxx");
 
 		//sets up music file to be played
@@ -102,7 +113,7 @@ public class TetrisPanel extends JPanel
 
 		//sets up panel
 		setPreferredSize (new Dimension(WIDTH, HEIGHT));
-		setBackground (Color.black);
+		setBackground (TetrisProperties.BACKGROUND_COLOR);
 		setFocusable(true);
 		DL = new DirectionListener();
 		addKeyListener (DL);
@@ -115,7 +126,7 @@ public class TetrisPanel extends JPanel
 
 	public void newBlock ()
 	{
-		int randBlock = rand.nextInt(7);
+		int randBlock = rand.nextInt(NUM_UNIQUE_TETRIS_BLOCKS);
 
 		switch (randBlock)
 		{
@@ -493,7 +504,7 @@ public class TetrisPanel extends JPanel
 		for (int j = 0; j < Board[0].length; j++)
 			Board[0][j] = new GridBlock();
 
-		score += 100;
+		score += SCORE_INCREMENT_VALUE;
 	}
 
 	public boolean GameOver ()
@@ -589,9 +600,9 @@ public class TetrisPanel extends JPanel
 			}
 		}
 
-		for (int j = 0; j < Board[21].length; j++)
+		for (int j = 0; j < Board[ROWS+1].length; j++)
 		{
-			Board[21][j].fill();
+			Board[ROWS+1][j].fill();
 		}
 
 		//restarts music
@@ -614,7 +625,7 @@ public class TetrisPanel extends JPanel
 		//reads in current high scores from the TetrisHighScores file
 		try
 		{
-			Scanner scan = new Scanner(new File("TetrisHighScores.txt"));
+			Scanner scan = new Scanner(new File(HIGH_SCORES_FILENAME));
 
 			String s1 = scan.next();
 			String s2 = scan.next();
@@ -680,7 +691,7 @@ public class TetrisPanel extends JPanel
 		}
 
 		//writes high scores to file TetrisHighScores
-		PrintWriter outFile = new PrintWriter (new FileWriter("TetrisHighScores.txt"));
+		PrintWriter outFile = new PrintWriter (new FileWriter(HIGH_SCORES_FILENAME));
 
 		//mini-header
 		outFile.println("Tetris High Scores");
@@ -706,7 +717,7 @@ public class TetrisPanel extends JPanel
 		blockHeight = boardHeight/ROWS;
 		moveY = blockHeight;
 
-		page.setColor(Color.white);
+		page.setColor(TEXT_COLOR);
 		page.setFont(scoreFont);
 		page.drawString("Score: " + score, 0, 15);
 		page.drawString("Level: " + level, 320, 15);
@@ -723,7 +734,7 @@ public class TetrisPanel extends JPanel
 				{
 					page.setColor(Board[i][j].getColor());
 					page.fillRect(j*blockWidth, (i - 1)*blockHeight, blockWidth, blockHeight);
-					page.setColor(Color.black);
+					page.setColor(TetrisProperties.BACKGROUND_COLOR);
 					page.drawRect(j*blockWidth, (i - 1)*blockHeight, blockWidth, blockHeight);
 				}
 			}
@@ -739,7 +750,7 @@ public class TetrisPanel extends JPanel
 					page.setColor(block.getColor());
 					page.fillRect((block.getX() + (j)*blockWidth), (block.getY() + i*blockHeight), blockWidth, 
 							blockHeight);
-					page.setColor(Color.black);
+					page.setColor(TetrisProperties.BACKGROUND_COLOR);
 					page.drawRect((block.getX() + (j)*blockWidth), (block.getY() + i*blockHeight), blockWidth, 
 							blockHeight);
 				}
@@ -780,7 +791,7 @@ public class TetrisPanel extends JPanel
 			timer.stop();
 			tetrisMusic.stop();
 			removeKeyListener (DL);
-			page.setColor(Color.white);
+			page.setColor(TEXT_COLOR);
 			page.setFont(gameOverFont);
 			page.drawString("PAUSED", boardWidth/5, boardHeight/2);
 		}
@@ -813,14 +824,14 @@ public class TetrisPanel extends JPanel
 					{
 						page.setColor(Board[1][j].getColor());
 						page.fillRect(j*blockWidth, (0)*blockHeight, blockWidth, blockHeight);
-						page.setColor(Color.black);
+						page.setColor(TetrisProperties.BACKGROUND_COLOR);
 						page.drawRect(j*blockWidth, (0)*blockHeight, blockWidth, blockHeight);
 					}
 				}
 
 				removeKeyListener (DL);
 				removeKeyListener (PL);
-				page.setColor(Color.white);
+				page.setColor(TEXT_COLOR);
 				page.setFont(gameOverFont);
 				page.drawString("GAME OVER!", 0, boardHeight/2);
 

src/main/java/com/tswift242/tetris/blocks/Iblock.java
@@ -68,12 +68,12 @@ public class Iblock extends TetrisBlock
 			template[0][3] = true;
 		}
 
-		orientation = ((orientation + 1) % 4);
+		orientation = ((orientation + 1) % BLOCK_BOX_SIZE);
 	}
 
 	public Boolean[][] getNextOrientArray ()
 	{
-		Boolean[][] a = new Boolean[4][4];
+		Boolean[][] a = new Boolean[BLOCK_BOX_SIZE][BLOCK_BOX_SIZE];
 
 		for (int i = 0; i < a.length; i++)
 		{

src/main/java/com/tswift242/tetris/blocks/Jblock.java
@@ -68,12 +68,12 @@ public class Jblock extends TetrisBlock
 			template[1][2] = true;
 		}
 
-		orientation = ((orientation + 1) % 4);
+		orientation = ((orientation + 1) % BLOCK_BOX_SIZE);
 	}
 
 	public Boolean[][] getNextOrientArray ()
 	{
-		Boolean[][] a = new Boolean[4][4];
+		Boolean[][] a = new Boolean[BLOCK_BOX_SIZE][BLOCK_BOX_SIZE];
 
 		for (int i = 0; i < a.length; i++)
 		{

src/main/java/com/tswift242/tetris/blocks/Lblock.java
@@ -68,12 +68,12 @@ public class Lblock extends TetrisBlock
 			template[1][2] = true;
 		}
 
-		orientation = ((orientation + 1) % 4);
+		orientation = ((orientation + 1) % BLOCK_BOX_SIZE);
 	}
 
 	public Boolean[][] getNextOrientArray ()
 	{
-		Boolean[][] a = new Boolean[4][4];
+		Boolean[][] a = new Boolean[BLOCK_BOX_SIZE][BLOCK_BOX_SIZE];
 
 		for (int i = 0; i < a.length; i++)
 		{

src/main/java/com/tswift242/tetris/blocks/Sblock.java
@@ -68,12 +68,12 @@ public class Sblock extends TetrisBlock
 			template[1][1] = true;
 		}
 
-		orientation = ((orientation + 1) % 4);
+		orientation = ((orientation + 1) % BLOCK_BOX_SIZE);
 	}
 
 	public Boolean[][] getNextOrientArray ()
 	{
-		Boolean[][] a = new Boolean[4][4];
+		Boolean[][] a = new Boolean[BLOCK_BOX_SIZE][BLOCK_BOX_SIZE];
 
 		for (int i = 0; i < a.length; i++)
 		{

src/main/java/com/tswift242/tetris/blocks/Tblock.java
@@ -68,12 +68,12 @@ public class Tblock extends TetrisBlock
 			template[1][2] = true;
 		}
 
-		orientation = ((orientation + 1) % 4);
+		orientation = ((orientation + 1) % BLOCK_BOX_SIZE);
 	}
 
 	public Boolean[][] getNextOrientArray ()
 	{
-		Boolean[][] a = new Boolean[4][4];
+		Boolean[][] a = new Boolean[BLOCK_BOX_SIZE][BLOCK_BOX_SIZE];
 
 		for (int i = 0; i < a.length; i++)
 		{

src/main/java/com/tswift242/tetris/blocks/TetrisBlock.java
@@ -2,15 +2,20 @@ package com.tswift242.tetris.blocks;
 
 import java.awt.Color;
 
+import com.tswift242.tetris.config.TetrisProperties;
+
 public abstract class TetrisBlock
 {
+	// TetrisBlock orientations are logically constrained to fit inside square boxes of this size
+	protected static final int BLOCK_BOX_SIZE = 4;
+
 	protected Boolean[][] template;
 	protected int x, y, orientation, curOrientLength, nextOrientLength;
 	protected Color shapeColor;
 
 	public TetrisBlock()
 	{
-		template = new Boolean[4][4];
+		template = new Boolean[BLOCK_BOX_SIZE][BLOCK_BOX_SIZE];
 		
 		for (int i = 0; i < template.length; i++)
 		{
@@ -21,7 +26,7 @@ public abstract class TetrisBlock
 		}
 
 		x = y = orientation = curOrientLength = nextOrientLength = 0;
-		shapeColor = Color.black;
+		shapeColor = TetrisProperties.BACKGROUND_COLOR;
 	}
 
 	public Boolean[][] getTemplate ()

src/main/java/com/tswift242/tetris/blocks/Zblock.java
@@ -68,12 +68,12 @@ public class Zblock extends TetrisBlock
 			template[1][2] = true;
 		}
 
-		orientation = ((orientation + 1) % 4);
+		orientation = ((orientation + 1) % BLOCK_BOX_SIZE);
 	}
 
 	public Boolean[][] getNextOrientArray ()
 	{
-		Boolean[][] a = new Boolean[4][4];
+		Boolean[][] a = new Boolean[BLOCK_BOX_SIZE][BLOCK_BOX_SIZE];
 
 		for (int i = 0; i < a.length; i++)
 		{

commit 4990aed787007c5809635fe2e059a756804bc16e

fix build error introduced by dcec42b

src/main/java/com/tswift242/tetris/TetrisPanel.java
@@ -44,7 +44,7 @@ public class TetrisPanel extends JPanel
 	private static final int INIT_DELAY = 400; //timer delay
 	// TODO: make this value formulaic instead of fixed in the future
 	private static final int SCORE_INCREMENT_VALUE = 100;
-	private static final int HIGH_SCORES_FILENAME = "TetrisHighScores.txt";
+	private static final String HIGH_SCORES_FILENAME = "TetrisHighScores.txt";
 	// maximum number of high scores to save
 	private static final int MAX_HIGH_SCORES = 10;
 	private static final String MUSIC_FILE = "/12-hans_zimmer-time.wav";